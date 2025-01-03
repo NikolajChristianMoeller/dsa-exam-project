22415aaea1d74efa84035c947fe7ac06461e93f9
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-01-03 22:03:45 +0100

    change some weights and values

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ let DP;
 let itemsAdded = [];
 
 const treasurePool = {
-  1: { img: "crown", value: 10, weight: 1 },
+  1: { img: "crown", value: 10, weight: 3 },
   2: { img: "dagger", value: 6, weight: 2 },
-  3: { img: "diamond_turkis", value: 15, weight: 3 },
+  3: { img: "diamond_turkis", value: 15, weight: 2 },
   4: { img: "golden_totem", value: 12, weight: 4 },
   5: { img: "golden_cup", value: 8, weight: 1 },
   6: { img: "golden_scull", value: 10, weight: 3 },
   7: { img: "golden_wine_glass", value: 9, weight: 1 },
-  8: { img: "money_with_sword", value: 11, weight: 2 },
-  9: { img: "ruby", value: 14, weight: 1 },
+  8: { img: "money_with_sword", value: 11, weight: 5 },
+  9: { img: "ruby", value: 14, weight: 2 },
   10: { img: "silver_wine_glass", value: 7, weight: 1 },
   11: { img: "sword", value: 9, weight: 4 },
 };
